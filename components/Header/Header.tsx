6d87daac6ad43de4529a5d6cfb75a2b8268655f5
Jeka <odinked@gmail.com>
2026-05-22 22:26:35 +0300

    feat: center logo in header, remove buy button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,10 @@
-'use client';
-
-import { useCheckout } from '@/components/Checkout/CheckoutProvider';
 import styles from './Header.module.css';
 
 export function Header() {
-  const { open } = useCheckout();
-
   return (
     <header className={styles.header}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src="/logo.png" alt="Саша Чемеров — Димна Суміш" className={styles.logo} />
-      <div className={styles.right}>
-        <span className={`${styles.drop} mono`}>DROP 01 // ONE SIZE (OVERSIZE)</span>
-        <button className={`${styles.buy} mono`} onClick={open}>Купити</button>
-      </div>
     </header>
   );
 }
